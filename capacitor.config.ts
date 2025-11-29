12da9e8b2f5139909c55cbb23f0d2687e336970b
miaoda <miaoda@baidu.com>
2025-11-28 21:05:13 -0300

    Configure Capacitor for Android with custom icons and splash screen

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.juniorcoelho.amigooculto',
+  appName: 'Amigo Oculto Mágico',
+  webDir: 'dist'
+};
+
+export default config;
